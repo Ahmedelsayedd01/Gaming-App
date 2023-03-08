554919f9b7ab1edf12c1047e6ff0183e0047cbcd
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-08 14:10:49 +0200

    New Update

---
+++ src/data.js
@@ -0,0 +1,109 @@
+/* Import Photo RightNow From Images  */
+import popular1 from "./assets/images/popular-01.jpg";
+import popular2 from "./assets/images/popular-02.jpg";
+import popular3 from "./assets/images/popular-03.jpg";
+import popular4 from "./assets/images/popular-04.jpg";
+import popular5 from "./assets/images/popular-05.jpg";
+import popular6 from "./assets/images/popular-06.jpg";
+import popular7 from "./assets/images/popular-07.jpg";
+import popular8 from "./assets/images/popular-08.jpg";
+/* Import Photo Library From Images  */
+import Library1 from "./assets/images/game-01.jpg";
+import Library2 from "./assets/images/game-02.jpg";
+import Library3 from "./assets/images/game-03.jpg";
+
+export const RightNow = [
+  {
+    id: 1,
+    image: popular1,
+    name: "Fortnite",
+    title: "Sandbox",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 2,
+    image: popular2,
+    name: "Pubg",
+    title: "Battle S",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 3,
+    image: popular3,
+    name: "Data2",
+    title: "Stream-X",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 4,
+    image: popular4,
+    name: "CS-GO",
+    title: "Legendary",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 5,
+    image: popular5,
+    name: "Mini Craft",
+    title: "Legendary",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 6,
+    image: popular6,
+    name: "Eagles Fly",
+    title: "Matrix Games",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 7,
+    image: popular7,
+    name: "Warface",
+    title: "Max 3D",
+    rate: 4.8,
+    download: "2.3M",
+  },
+  {
+    id: 8,
+    image: popular8,
+    name: "warcraft",
+    title: "Legend",
+    rate: 4.8,
+    download: "2.3M",
+  },
+];
+export const Library = [
+  {
+    id: 1,
+    image: Library1,
+    name: "Data 2",
+    title: "Sandbox",
+    date: "24/08/2036",
+    hours: "634 H 22 Mins",
+    currently: "Downloaded",
+  },
+  {
+    id: 2,
+    image: Library2,
+    name: "Fortnite",
+    title: "Sandbox",
+    date: "22/06/2036",
+    hours: "740 H 52 Mins",
+    currently: "Downloaded",
+  },
+  {
+    id: 3,
+    image: Library3,
+    name: "CS-GO",
+    title: "Sandbox",
+    date: "21/04/2036",
+    hours: "892 H 14 Mins",
+    currently: "Downloaded",
+  },
+];
