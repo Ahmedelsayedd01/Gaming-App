cca2caa9e6ac49bea4795266f9b4530d2d0ec3d1
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-08 14:10:18 +0200

    New Update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <>
+      <Nav className="" />
+      <div className="w-10/12 mx-auto"></div>
+    </>
+  );
+};
+
+export default Home;
