ed120ddf2179277c541f49d0fc0bb49176451a02
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-09 13:29:31 +0200

    New Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import SectionRight from "../components/SectionRight";
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <Header />
+      <div className="w-[85%] mx-auto mt-4">
+        <div className="bg-[#27292a] rounded-3xl mx-auto px-8 sl:p-14">
+          {/* casteism Section */}
+          <Header />
+          <SectionRight />
+        </div>
+      </div>
     </>
   );
 };
