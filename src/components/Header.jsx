8a5af5775721e9eb5dc01c601f9c4d4bbc9fc6b3
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-09 13:28:18 +0200

    New Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,17 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-8/12 mx-auto">
-      <div className="bg-[#27292a] rounded-2xl mt-8 mx-auto px-8">
-        <div className="flex items-center justify-center bg-[#212529] rounded-2xl ">
-          <div className="flex items-center justify-center bg-header-pattern rounded-2xl object-cover bg-center w-full h-full">
-            <div className="w-5/12 mt-8">
-              <span className="font-light ">Welcome To Cyborg</span>
-              <h3 className="flex-wrap mt-8 font-bold text-4xl">
-                <em>BROWSE</em> OUR POPULAR GAMES HERE
-              </h3>
-              <button className="btn">
-                <Link to={"/browse"}>Browse Now</Link>
-              </button>
-            </div>
-            <div className="hidden"></div>
-          </div>
-        </div>
+    <div className="flex items-center justify-center bg-header-pattern rounded-3xl bg-cover bg-center w-full h-full sl:justify-start sl:px-16">
+      <div className="w-9/12 sl:w-7/12 md:w-5/12 my-[80px] px-[60] flex flex-col items-start justify-center">
+        <h4 className="font-normal text-xl mb-8">Welcome To Cyborg</h4>
+        <h3 className="flex-wrap mb-8 font-bold text-4xl">
+          <span className="text-[#ec6090] ">BROWSE</span> OUR POPULAR GAMES HERE
+        </h3>
+        <button className="btn">
+          <Link to={"/browse"}>Browse Now</Link>
+        </button>
       </div>
+      <div className="sm:hidden sl:block"></div>
     </div>
   );
 };
