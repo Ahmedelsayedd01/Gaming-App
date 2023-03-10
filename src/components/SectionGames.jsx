116057873d1953cbe0da224e93f5c9c33ca4a661
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-10 15:10:57 +0200

    New Update

--- src/components/SectionGames.jsx
+++ src/components/SectionGames.jsx
@@ -15,9 +15,9 @@ import { FaDownload } from "react-icons/fa";
 const SectionGames = () => {
   const swiper = useSwiper();
   return (
-    <div className="flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto">
-      <div className="px-5 w-full flex flex-col items-center justify-center">
-        <div className="flex py-4 mb-3 items-start  w-full text-2xl font-bold">
+    <div className=" bg-[#1f2122] rounded-3xl mx-auto p-8">
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="flex py-4 mb-3 items-start  w-full text-3xl font-bold">
           <h3 className="mr-1 underline decoration-2">Featured</h3>
           <h4 className="text-[#ec6090]">Games</h4>
           <div className=" w-full flex justify-end items-center">
@@ -29,56 +29,52 @@ const SectionGames = () => {
           <Swiper
             modules={[Navigation, A11y]}
             spaceBetween={30}
-            slidesPerView={2}
+            slidesPerView={3}
             navigation
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            <button className="text-red-50 w-full bg-slate-100" onClick={() => swiper.slideNext()}>
+            {/* <button className="text-red-50 w-full bg-slate-100" onClick={() => swiper.slideNext()}>
               <GrNext className=""/>
-            </button>
+            </button> */}
             <div className="">
               {FeaturedGames.map((card) => (
-                <div className="">
-                  <SwiperSlide key={card.id}>
-                    <div className="hover:cursor-grab">
-                      <div className="relative flex items-center justify-center group transition-all ease-in-out duration-300 overflow-hidden">
-                        <img
-                          src={card.image}
-                          alt=""
-                          className="w-full rounded-3xl bg-cover bg-center"
-                        />
-                        <span className="absolute -bottom-8 group-hover:bottom-5 transition-all ease-in-out duration-300 text-[#ec6090] bg-[rgba(31,33,34,0.95)] px-3 py-1 rounded-full text-base font-medium ">
-                          {card.streaming}
+                <SwiperSlide className="flex hover:cursor-grab" key={card.id}>
+                  <div className="relative  flex items-center justify-center group transition-all ease-in-out duration-300 overflow-hidden">
+                    <img
+                      src={card.image}
+                      alt=""
+                      className="w-full rounded-3xl bg-cover bg-center"
+                    />
+                    <span className="absolute -bottom-8 group-hover:bottom-5 transition-all ease-in-out duration-300 text-[#ec6090] bg-[rgba(31,33,34,0.95)] px-3 py-1 rounded-full text-base font-medium ">
+                      {card.streaming}
+                    </span>
+                  </div>
+                  <div className="flex items-center justify-between py-4">
+                    <ul>
+                      <li className="text-white text-base font-bold">
+                        {card.name}
+                      </li>
+                      <li className="text-[#666] text-lg font-normal mt-2">
+                        {card.title}
+                      </li>
+                    </ul>
+                    <ul>
+                      <li className="flex items-center text-white text-base font-light">
+                        <span className="text-yellow-300 mr-1">
+                          <AiFillStar />
                         </span>
-                      </div>
-                      <div className="flex items-center justify-between py-4">
-                        <ul>
-                          <li className="text-white text-base font-bold">
-                            {card.name}
-                          </li>
-                          <li className="text-[#666] text-lg font-normal mt-2">
-                            {card.title}
-                          </li>
-                        </ul>
-                        <ul>
-                          <li className="flex items-center text-white text-base font-light">
-                            <span className="text-yellow-300 mr-1">
-                              <AiFillStar />
-                            </span>
-                            {card.rate}
-                          </li>
-                          <li className="flex items-center text-white text-base font-light">
-                            <span className="text-[#ec6090] mr-1">
-                              <FaDownload />
-                            </span>
-                            {card.download}
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                </div>
+                        {card.rate}
+                      </li>
+                      <li className="flex items-center text-white text-base font-light">
+                        <span className="text-[#ec6090] mr-1">
+                          <FaDownload />
+                        </span>
+                        {card.download}
+                      </li>
+                    </ul>
+                  </div>
+                </SwiperSlide>
               ))}
             </div>
           </Swiper>
