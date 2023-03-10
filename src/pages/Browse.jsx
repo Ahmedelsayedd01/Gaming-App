bccedb3566f9960c6ee18bb2c3afdd76ea29a3af
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-10 22:34:58 +0200

    New Update

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -8,10 +8,10 @@ const Browse = () => {
         <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
           {/* casteism Section */}
           <div className="flex sm:flex-col md:flex-row md:gap-x-5 w-full items-center md:justify-between ">
-            <div className="md:w-4/6">
+            <div className="sm:w-full md:w-4/6">
               <SectionGames />
             </div>
-            <div className="md:w-2/6">
+            <div className="sm:w-full md:w-2/6">
               <SectionDownloaded />
             </div>
           </div>
