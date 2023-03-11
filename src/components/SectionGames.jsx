e7dd6e93999343ee08f742a0713a2f8471863aac
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-11 22:53:32 +0200

    New Update

--- src/components/SectionGames.jsx
+++ src/components/SectionGames.jsx
@@ -17,10 +17,11 @@ const SectionGames = () => {
   return (
     <div className=" bg-[#1f2122] rounded-3xl mx-auto p-8 sm:mb-14 md:mb-0">
       <div className="relative w-full flex flex-col items-center justify-center">
-        <div className=" flex pb-4 mb-3 items-start  w-full text-3xl font-bold">
-          <h3 className="mr-1 underline decoration-2">Featured</h3>
-          <h4 className="text-[#ec6090]">Games</h4>
-          <div className=" w-full flex justify-end items-center"></div>
+        <div className=" flex pb-4 mb-3 items-start justify-between  w-full text-3xl font-bold">
+          <div className="flex">
+            <h3 className="mr-1 underline decoration-2">Featured</h3>
+            <h4 className="text-[#ec6090]">Games</h4>
+          </div>
           <div className="flex">
             <button className="text-2xl text-[#666] hover:text-[#ec6090] transition-all ease-in-out duration-300">
               <MdOutlineNavigateBefore />
