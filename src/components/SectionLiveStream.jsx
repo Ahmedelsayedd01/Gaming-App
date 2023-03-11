419ad468e3380e6edeb1e4614c7a8be1e61872c7
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-11 22:53:19 +0200

    New Update

---
+++ src/components/SectionLiveStream.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SectionLiveStream = () => {
+  return (
+    <div className=" bg-[#1f2122] rounded-3xl mx-auto p-8 sm:mb-14 md:mb-0">
+      <div className="relative w-full flex flex-col items-center justify-center">
+        <div className=" flex pb-4 mb-3 items-start justify-between  w-full text-3xl font-bold">
+          <div className="flex">
+            <h3 className="mr-1 underline decoration-2">How To Start Your</h3>
+            <h4 className="text-[#ec6090]">Live Stream</h4>
+          </div>
+        </div>
+        <div className="w-full relative"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionLiveStream;
