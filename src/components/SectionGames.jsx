0097cc7a064a126fff4c43a2e414c0da77b344a6
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-11 13:37:10 +0200

    New Update

--- src/components/SectionGames.jsx
+++ src/components/SectionGames.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FeaturedGames } from "../data";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Navigation, A11y, Autoplay } from "swiper";
 
@@ -9,21 +9,20 @@ import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
 // import { Link } from "react-router-dom";
-import { GrNext } from "react-icons/gr";
 import "swiper/css";
 import { AiFillStar } from "react-icons/ai";
 import { FaDownload } from "react-icons/fa";
+import { SlideNextButton } from "./SlideNextButton";
 const SectionGames = () => {
-  const swiper = useSwiper();
   return (
     <div className=" bg-[#1f2122] rounded-3xl mx-auto p-8 sm:mb-14 md:mb-0">
-      <div className="w-full flex flex-col items-center justify-center">
-        <div className="flex pb-4 mb-3 items-start  w-full text-3xl font-bold">
+      <div className="relative w-full flex flex-col items-center justify-center">
+        <div className=" flex pb-4 mb-3 items-start  w-full text-3xl font-bold">
           <h3 className="mr-1 underline decoration-2">Featured</h3>
           <h4 className="text-[#ec6090]">Games</h4>
           <div className=" w-full flex justify-end items-center"></div>
         </div>
-        <div className="w-full overflow-hidden ">
+        <div className="w-full relative">
           <Swiper
             modules={[Navigation, A11y, Autoplay]}
             loop={true}
@@ -35,22 +34,15 @@ const SectionGames = () => {
               740: { slidesPerView: 2 },
               1270: { slidesPerView: 3 },
             }}
-            autoplay={{ delay: 1500 }}
-            // onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
+            // autoplay={{ delay: 1500 }}
+            className=""
           >
-            <div className="flex items-center justify-center">
-              <button
-                className=" text-yellow-300"
-                onClick={() => swiper.slideNext()}
-              >
-                <AiFillStar className="" />
-              </button>
-            </div>
+            <SlideNextButton />
+            <div className="flex items-center justify-center"></div>
             <div className="">
               {FeaturedGames.map((card) => (
                 <SwiperSlide className="flex hover:cursor-grab" key={card.id}>
-                  <div className="relative  flex items-center justify-center group transition-all ease-in-out duration-300 overflow-hidden">
+                  <div className="relative overflow-hidden flex items-center justify-center group transition-all ease-in-out duration-300">
                     <img
                       src={card.image}
                       alt=""
@@ -127,8 +119,6 @@ const SectionGames = () => {
             </div>
           </Swiper>
         </div>
-
-        {/* <div className="grid sm:grid-cols-1 sl:grid-cols-2  md:grid-cols-4 gap-6 w-full mb-8"></div> */}
       </div>
     </div>
   );
