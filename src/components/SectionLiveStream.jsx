d1bf9360abe52da66e99578d5d753e104e231cb7
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-11 23:08:02 +0200

    New Update

--- src/components/SectionLiveStream.jsx
+++ src/components/SectionLiveStream.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { LiveStream } from "../data";
 
 const SectionLiveStream = () => {
   return (
-    <div className="bg-transparent mx-auto p-12 sm:mb-14 md:mb-0">
-      <div className="relative w-full flex flex-col items-center justify-center">
-        <div className=" flex  items-start justify-center  w-full text-3xl font-bold">
-          <div className="flex">
-            <h3 className="mr-1 underline decoration-2">How To Start Your</h3>
-            <h4 className="text-[#ec6090]">Live Stream</h4>
-          </div>
+    <div className="bg-transparent mx-auto sm:mb-14 md:mb-0">
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className=" flex p-12 items-start justify-center  w-full text-3xl font-bold">
+          <h3 className="mr-1 underline decoration-2">How To Start Your</h3>
+          <h4 className="text-[#ec6090]">Live Stream</h4>
         </div>
+        <div className="">{LiveStream.map((card)=>(
+            <div className="">
+                <div className="">{card.image}</div>
+                <div className="">{card.name}</div>
+                <div className="">{card.title}</div>
+            </div>
+        ))}</div>
       </div>
     </div>
   );
