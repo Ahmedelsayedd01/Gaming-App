8ff5a956638cfe5622c115d289d953f369b37384
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-12 14:53:52 +0200

    New Update

--- src/components/SectionLiveStream2.jsx
+++ src/components/SectionLiveStream2.jsx
@@ -7,31 +7,42 @@ const SectionLiveStream2 = () => {
   return (
     <div className="flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16">
       <div className="p-8 w-full flex flex-col items-center justify-center">
-        <div className="flex pb-4 mb-3 items-start w-full text-4xl font-bold">
+        <div className="flex pb-4 mb-3 items-start w-full text-xl sl:text-4xl font-bold">
           <h3 className="mr-1 underline decoration-2">Most Popular</h3>
-          <h4 className="text-[#ec6090]">Right Now</h4>
+          <h4 className="text-[#ec6090]">Live Stream</h4>
         </div>
-        <div className="flex justify-between w-full gap-5">
+        <div className="w-full gap-5 pb-6 grid sm:grid-cols-1 sl:grid-cols-2 md:grid-cols-4">
           {LiveStreamGames.map((card) => (
-            <div className="w-full h-full pb-5 " key={card.id}>
+            <div className="w-full h-full" key={card.id}>
               {/* Top Div */}
-              <div className="">
+              <div className="relative">
                 <img
                   src={card.image}
                   alt=""
                   className="rounded-3xl bg-cover bg-center w-full"
                 />
+                <div className="absolute top-0">
+                  <h3>Live</h3>
+                </div>
+                <div className="flex items-center justify-between absolute bottom-0">
+                  <h3 className="btn-s">
+                    <span></span>1.2K
+                  </h3>
+                  <h3 className="btn-s">
+                    <span></span>CS-GO
+                  </h3>
+                </div>
               </div>
               {/* Bottom Div */}
-              <div className="pt-4 flex w-full justify-between items-start">
+              <div className="pt-6 flex w-full justify-start items-start">
                 {/* Left Div */}
                 <div className="">
                   <img src={card.profileImg} alt="" className="rounded-full" />
                 </div>
                 {/* Right Div */}
-                <div className="flex flex-col ml-4 items-start justify-start">
+                <div className="flex flex-col ml-3 items-start justify-start">
                   <div className="flex items-center justify-start">
-                    <span className="bg-[#ec6090] rounded-full items-center mr-1 text-xl font-thin">
+                    <span className="bg-[#ec6090] rounded-full items-center mr-2 text-xl font-thin">
                       <BiCheck />
                     </span>
                     <h3 className="text-[#ec6090] text-sm">{card.name}</h3>
