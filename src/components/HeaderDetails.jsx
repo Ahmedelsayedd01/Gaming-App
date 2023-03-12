7beb31a61494eed51afe962c1bc4ebec2121bad1
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-12 19:25:34 +0200

    New Update

--- src/components/HeaderDetails.jsx
+++ src/components/HeaderDetails.jsx
@@ -5,10 +5,18 @@ const HeaderDetails = () => {
   return (
     <div className="w-full flex sm:flex-col sl:flex-row gap-y-5 sl:gap-x-9  justify-between ">
       <div className="w-auto">
-        <img src={featureLeft} alt="" className="w-full h-full bg-cover bg-center rounded-3xl"/>
+        <img
+          src={featureLeft}
+          alt=""
+          className="w-full h-full bg-cover bg-center rounded-3xl"
+        />
       </div>
       <div className="sl:w-8/12 ">
-        <img src={featureRight} alt="" className="w-full h-full bg-cover bg-center rounded-3xl"/>
+        <img
+          src={featureRight}
+          alt=""
+          className="w-full h-full bg-cover bg-center rounded-3xl"
+        />
       </div>
     </div>
   );
