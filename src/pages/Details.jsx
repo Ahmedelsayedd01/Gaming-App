108804d04c5219c4201caef060562c8ac92dbe48
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-12 17:35:05 +0200

    New Update

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import HeaderDetails from "../components/HeaderDetails";
 
 const Details = () => {
   return (
-    <div>Details</div>
-  )
-}
+    <>
+      <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
+        <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
+          <HeaderDetails />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Details
+export default Details;
