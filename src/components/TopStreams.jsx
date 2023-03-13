9bc54141bedc34e53209d657abb1fc2822a1af96
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-13 16:18:57 +0200

    New Update

--- src/components/TopStreams.jsx
+++ src/components/TopStreams.jsx
@@ -1,74 +1,55 @@
 import React from "react";
-import { TopDownloaded } from "../data";
-import { AiFillStar } from "react-icons/ai";
-import { FaDownload } from "react-icons/fa";
+import { TopStreamers } from "../data";
+import { BiCheck } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const TopStreams = () => {
   return (
-    <div className="bg-[#1f2122] rounded-3xl mx-auto py-8 px-6">
+    <div className="bg-[#1f2122] rounded-3xl mx-auto p-7 pb-0 sm:mb-14 md:mb-0">
       <div className="w-full flex flex-col items-center justify-center ">
         <div className="flex w-full sl:flex-wrap pb-4  items-start sm:text-4xl font-bold ">
           <h3 className="mr-1 underline decoration-2">Top</h3>
-          <h4 className="text-[#ec6090]">Downloaded</h4>
+          <h4 className="text-[#ec6090]">Streamers</h4>
         </div>
-
-        <div className="">
-          {TopDownloaded.map((card) => (
+        {/* TopStreamers */}
+        <div className="w-full">
+          {TopStreamers.map((card) => (
             <div
-              className="grid sm:grid-cols-3 sm:grid-rows-1 md:grid-cols-3 md:grid-rows-1 py-5 gap-x-2 w-full place-items-center border-b border-[#404345]"
+              className="w-full flex sm:flex-col sl:flex-row md:flex-col xl:flex-row gap-y-3 lg:gap-y-0 sl:justify-between sm:items-start sl:items-center py-6 border-b border-[#4c4d4e] last:border-none"
               key={card.id}
             >
-              <div className="w-full">
-                <img
-                  src={card.image}
-                  alt=""
-                  className="rounded-3xl bg-cover bg-center w-auto"
-                />
+              {/* Left */}
+              <div className="flex items-center justify-start sm:w-full  gap-x-4 ">
+                <div className="">
+                  <h3 className="font-bold">0{card.number}</h3>
+                </div>
+                <div className="">
+                  <img src={card.image} alt="" className="rounded-full" />
+                </div>
+                <div className="">
+                  <ul className="flex items-center justify-start">
+                    <li className="bg-[#ec6090] rounded-full items-center mr-2 text-xl font-thin">
+                      <BiCheck />
+                    </li>
+                    <li className="text-[#ec6090] text-sm">{card.name}</li>
+                  </ul>
+                </div>
               </div>
-              <div className="w-full">
-                <ul className="w-full flex flex-col items-center">
-                  <li className="text-white text-base font-bold ">
-                    {card.name}
-                  </li>
-                  <li className="text-[#666] text-lg font-normal mt-2">
-                    {card.title}
-                  </li>
-                  <li className="flex items-center justify-center w-full gap-x-3">
-                    <div className="">
-                      <ul className="flex items-center text-white text-base font-light mt-2">
-                        <li className="text-yellow-300 mr-1">
-                          <AiFillStar />
-                        </li>
-                        <li>{card.rate}</li>
-                      </ul>
-                    </div>
-
-                    <div>
-                      <ul className="flex items-center text-white text-base font-light mt-2">
-                        <li className="text-[#ec6090] mr-1">
-                          <FaDownload />
-                        </li>
-                        <li>{card.download}</li>
-                      </ul>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-              <div className="w-2/6 flex items-center sm:justify-end sl:justify-center md:justify-end text-[#ec6090]">
-                <FaDownload />
+              {/* Right */}
+              <div className="sm:w-full sl:w-auto md:w-full xl:w-auto flex justify-center items-center">
+                <Link
+                  to={"/steam"}
+                  className="btn py-2 flex justify-center w-full"
+                >
+                  Follow
+                </Link>
               </div>
             </div>
           ))}
         </div>
-        <div className="pt-8 pb-4">
-          <Link
-            to={"/profile"}
-            className="text-xl text-[#e75e8d] font-semibold hover:text-white transition-all ease-in-out duration-300"
-          >
-            View All Games
-          </Link>
-        </div>
+        {/* <div className="pt-8 pb-4">
+          
+        </div> */}
       </div>
     </div>
   );
