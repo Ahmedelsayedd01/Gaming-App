e73bc1ebed3594e3d855449a44780f6b9bebffeb
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-13 14:08:47 +0200

    New Update

--- src/pages/Streams.jsx
+++ src/pages/Streams.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import LiveStreams from "../components/LiveStreams";
+import SectionDownloaded from "../components/SectionDownloaded";
+import SectionGames from "../components/SectionGames";
+import TopStreams from "../components/TopStreams";
 
 const Streams = () => {
-  return <div>Streams</div>;
+  return (
+    <>
+      <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
+        <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
+          {/* casteism Section */}
+          <div className="flex sm:flex-col md:flex-row md:gap-x-5 w-full items-center md:justify-between ">
+            <div className="sm:w-full md:w-4/6">
+              {/* <SectionGames /> */}
+              <LiveStreams />
+            </div>
+            <div className="sm:w-full md:w-2/6">
+              {/* <SectionDownloaded /> */}
+              <TopStreams />
+            </div>
+          </div>
+          {/* <SectionLiveStream /> */}
+          {/* <SectionLiveStream2 /> */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Streams;
