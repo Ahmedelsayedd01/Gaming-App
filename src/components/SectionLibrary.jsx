f35fe34bf395627513e98efca56bf23e3134d7c3
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 23:53:23 +0200

    New Update

--- src/components/SectionLibrary.jsx
+++ src/components/SectionLibrary.jsx
@@ -5,14 +5,14 @@ const SectionLibrary = () => {
   return (
     <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16">
       <div className="px-5 w-full flex flex-col items-center justify-center">
-        <div className="flex py-4 mb-3 items-start w-full text-2xl font-bold">
+        <div className="flex pt-4 items-start w-full text-3xl font-bold">
           <h3 className="mr-1 underline decoration-2">Your Gaming</h3>
           <h4 className="text-[#ec6090]">Library</h4>
         </div>
-        <div className="flex flex-col justify-around gap-2 w-full mb-8">
+        <div className="flex flex-col justify-around  w-full mb-4">
           {Library.map((item) => (
             <div
-              className="grid sm:grid-rows-3 sm:grid-cols-2 sl:grid-rows-2 sl:grid-cols-3 md:grid-rows-1 md:grid-cols-6 w-full place-items-center  bg-transparent border-b border-[#4c4d4e] py-6"
+              className="grid sm:grid-rows-3 sm:grid-cols-2 sl:grid-rows-2 sl:grid-cols-3 md:grid-rows-1 md:grid-cols-6 w-full place-items-center  bg-transparent border-b border-[#4c4d4e] last:border-none py-6"
               key={item.id}
             >
               <div className="flex items-center md:justify-start sm:justify-center  w-full">
