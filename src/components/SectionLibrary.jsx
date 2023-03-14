7db7ce73c2d71575ec7fe486d9cb44f8e04514f7
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-15 00:33:23 +0200

    New Update

--- src/components/SectionLibrary.jsx
+++ src/components/SectionLibrary.jsx
@@ -3,13 +3,13 @@ import { Library } from "../data";
 import { Link } from "react-router-dom";
 const SectionLibrary = () => {
   return (
-    <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto ">
+    <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16 md:p-4">
       <div className="px-5 w-full flex flex-col items-center justify-center">
         <div className="flex pt-4 items-start w-full text-4xl font-bold">
           <h3 className="mr-1 underline decoration-2">Your Gaming</h3>
           <h4 className="text-[#ec6090]">Library</h4>
         </div>
-        <div className="flex flex-col justify-around  w-full mb-4">
+        <div className="flex flex-col justify-around  w-full">
           {Library.map((item) => (
             <div
               className="grid sm:grid-rows-3 sm:grid-cols-2 sl:grid-rows-3 sl:grid-cols-2 md:grid-rows-2 md:grid-cols-3 lg:grid-rows-1 lg:grid-cols-6 w-full place-items-center  bg-transparent border-b border-[#4c4d4e] last:border-none py-6"
@@ -22,17 +22,19 @@ const SectionLibrary = () => {
                   className="rounded-3xl bg-cover bg-center w-auto"
                 />
               </div>
-              <div className="gap-1 sm:mt-2 sl:mt-1 md:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-start">
+              <div className="gap-1 sm:mt-2 sl:mt-1 md:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-center lg:items-start">
                 <h3 className="">{item.name}</h3>
                 <span className="text-[#666] text-lg font-normal">
                   {item.title}
                 </span>
               </div>
-              <div className="gap-1 sm:mt-2 sl:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-start">
-                <h3>{item.des}</h3>
-                <span className="text-[#666] text-lg font-normal">
-                  {item.download}
-                </span>
+              <div className="gap-1 sm:mt-2 sl:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-end lg:items-start">
+                <div className="flex flex-col sm:items-center md:items-center">
+                  <h3>{item.des}</h3>
+                  <span className="text-[#666] text-lg font-normal">
+                    {item.download}
+                  </span>
+                </div>
               </div>
               <div className="gap-1 sm:mt-2 sl:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-start">
                 <h3>Date Added</h3>
@@ -40,7 +42,7 @@ const SectionLibrary = () => {
                   {item.date}
                 </span>
               </div>
-              <div className="gap-1 sm:mt-2 sl:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-start">
+              <div className="gap-1 sm:mt-2 sl:mt-0 text-white text-base font-bold w-full flex flex-col sm:items-center md:items-center lg:items-start ">
                 <h3>Hours Played</h3>
                 <span className="text-[#666] text-lg font-normal">
                   {item.hours}
@@ -54,7 +56,7 @@ const SectionLibrary = () => {
             </div>
           ))}
         </div>
-        <div className="-mb-6">
+        <div className="-mb-10">
           <button className="btn">
             <Link to={"/profile"}>View Your Library</Link>
           </button>
