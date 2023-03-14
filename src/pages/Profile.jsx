794f4c935cc415b656666d69e06fea50a1f830ae
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 22:24:11 +0200

    New Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import HeaderProfile from "../components/HeaderProfile";
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
+        <div className="bg-transparent rounded-3xl mx-auto sm:px-6 sl:p-12">
+          <HeaderProfile />
 
-export default Profile
+          
+          <div className="flex sm:flex-col md:flex-row md:gap-x-5 w-full items-center md:justify-between ">
+            <div className="sm:w-full md:w-4/6">{/* <LiveProfile /> */}</div>
+            <div className="sm:w-full md:w-2/6">{/* <TopProfile /> */}</div>
+          </div>
+          {/* <MostStreamers /> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
