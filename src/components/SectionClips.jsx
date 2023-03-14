2b761f6641ffcf3e501392b4839b8f07cfabaefd
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 23:45:48 +0200

    New Update

--- src/components/SectionClips.jsx
+++ src/components/SectionClips.jsx
@@ -6,12 +6,12 @@ import { FaPlay, FaEye } from "react-icons/fa";
 const SectionClips = () => {
   return (
     <div className="flex items-center justify-center flex-col bg-transparent pb-5 rounded-3xl mx-auto">
-      <div className="pt-8 w-full flex flex-col items-center justify-center">
+      <div className="pt-6 w-full flex flex-col items-center justify-center">
         <div className="flex mb-4 items-start w-full sl:text-4xl font-bold">
           <h3 className="mr-1 underline decoration-2">Your Most Popular</h3>
           <h4 className="text-[#ec6090]">Clips</h4>
         </div>
-        <div className="w-full gap-5 pb-6 grid sm:grid-cols-1 sl:grid-cols-2 md:grid-cols-4">
+        <div className="w-full gap-5 pb-6 grid sm:grid-cols-1 sl:grid-cols-2 md:grid-cols-4 mt-5">
           {Clips.map((card) => (
             <div className="bg-[#27292a] py-8 px-4 rounded-3xl">
               {/* top */}
@@ -55,7 +55,7 @@ const SectionClips = () => {
           ))}
         </div>
 
-        <div className="">
+        <div className="p-6">
           <button className="btn">
             <Link to={"/profile"}>Load More Clips</Link>
           </button>
