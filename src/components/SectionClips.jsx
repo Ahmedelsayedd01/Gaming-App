82831273808ddfe6e824eceefaf272e699c17afc
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 22:57:12 +0200

    New Update

--- src/components/SectionClips.jsx
+++ src/components/SectionClips.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const SectionClips = () => {
   return (
-    <div className="flex items-center justify-center flex-col bg-transparent rounded-3xl mx-auto">
+    <div className="flex items-center justify-center flex-col bg-transparent pb-5 rounded-3xl mx-auto">
       <div className="pt-8 w-full flex flex-col items-center justify-center">
         <div className="flex mb-4 items-start w-full sl:text-4xl font-bold">
           <h3 className="mr-1 underline decoration-2">Your Most Popular</h3>
           <h4 className="text-[#ec6090]">Clips</h4>
         </div>
         <div className="w-full gap-5 pb-6 grid sm:grid-cols-1 sl:grid-cols-2 md:grid-cols-4">
-          sadsadsad
+          
         </div>
 
-        <div className="-mb-14">
+        <div className="">
           <button className="btn">
-            <Link to={"/stream"}>Discover All Streams</Link>
+            <Link to={"/profile"}>Load More Clips</Link>
           </button>
         </div>
       </div>
