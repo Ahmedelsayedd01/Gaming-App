e3f0db01012dea36476c2ed93d1a0ebb7272a549
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 22:55:18 +0200

    New Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import HeaderProfile from "../components/HeaderProfile";
+import SectionClips from "../components/SectionClips";
 const Profile = () => {
   return (
     <>
       <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
-        <div className="bg-transparent rounded-3xl mx-auto sm:px-6 sl:p-12">
-          <HeaderProfile />
+        <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
+          <div className="bg-[#1f2122] rounded-3xl mx-auto p-7 pb-0 sm:mb-14 md:mb-0">
+            <HeaderProfile />
+            <SectionClips />
+          </div>
 
-          
           <div className="flex sm:flex-col md:flex-row md:gap-x-5 w-full items-center md:justify-between ">
             <div className="sm:w-full md:w-4/6">{/* <LiveProfile /> */}</div>
             <div className="sm:w-full md:w-2/6">{/* <TopProfile /> */}</div>
