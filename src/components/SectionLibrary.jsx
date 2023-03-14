4d34ec304b0118f3f8d13fad629a100c8ee39aba
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-14 23:58:43 +0200

    New Update

--- src/components/SectionLibrary.jsx
+++ src/components/SectionLibrary.jsx
@@ -3,7 +3,7 @@ import { Library } from "../data";
 import { Link } from "react-router-dom";
 const SectionLibrary = () => {
   return (
-    <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16">
+    <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto ">
       <div className="px-5 w-full flex flex-col items-center justify-center">
         <div className="flex pt-4 items-start w-full text-3xl font-bold">
           <h3 className="mr-1 underline decoration-2">Your Gaming</h3>
