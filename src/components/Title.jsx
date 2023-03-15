cd829d7ad9a7932c216497a25fd4a3e8e38bb2e4
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-15 14:56:13 +0200

    New Update

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React, { Children } from "react";
+
+const Title = (props) => {
+  return (
+    <div className="flex items-start w-full sm:text-2xl sl:text-4xl font-bold">
+      <h3 className="mr-1 underline decoration-2">{props.Text1}</h3>
+      <h4 className="text-[#ec6090]">{props.Text2}</h4>
+    </div>
+  );
+};
+
+export default Title;
