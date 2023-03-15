b00423c5837b6049ea82327e5498c9d9c528bcf6
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-15 14:55:04 +0200

    New Update

--- src/components/DetailsGames.jsx
+++ src/components/DetailsGames.jsx
@@ -2,28 +2,22 @@ import React from "react";
 import { AiFillStar } from "react-icons/ai";
 import { FaDownload } from "react-icons/fa";
 import { DetailsItem } from "../data";
+import Title from "./Title";
 
 const DetailsGames = () => {
   return (
     <div className="w-full flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16">
-      <div className="px-5 w-full flex flex-col items-center justify-center">
-        <div className="flex py-4 mb-3 items-start w-full text-3xl font-bold">
-          <h3 className="mr-1 underline decoration-2">Other Related</h3>
-          <h4 className="text-[#ec6090]">Games</h4>
-        </div>
+      <div className="px-5 w-full flex flex-col items-center justify-center py-8">
+        <Title Text1="Other Related" Text2="Games" />
         <div className=" w-full grid sm:grid-cols-1 sl:grid-cols-2 gap-x-5">
           {DetailsItem.map((item) => (
             <div
-              className="  flex justify-between item-center  w-full border-b border-[#4c4d4e] pb-7 mb-8"
+              className="flex justify-between item-center w-full border-b border-[#4c4d4e] py-6 "
               key={item.id}
             >
               {/* Left */}
               <div className="">
-                <img
-                  src={item.image}
-                  alt=""
-                  className="bg-cover bg-center rounded-2xl"
-                />
+                <img src={item.image} alt="" className="rounded-2xl" />
               </div>
               {/* Center */}
               <div className="flex items-center justify-start w-3/4 ml-2">
