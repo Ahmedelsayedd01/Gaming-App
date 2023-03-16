d49b69c80d71ae07d0fc9ad1f46c72e8821248f8
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-16 23:44:24 +0200

    New Update

--- src/components/MostStreamers.jsx
+++ src/components/MostStreamers.jsx
@@ -1,55 +1,46 @@
 import React from "react";
-import { BiCheck } from "react-icons/bi";
-import { FaRegEye } from "react-icons/fa";
-import { IoLogoGameControllerA } from "react-icons/io";
-import { Link } from "react-router-dom";
 import { Streamers } from "../data";
+import Live from "./Live";
 import Button from "./Button";
 import Title from "./Title";
-
+import ActiveStreamers from "./ActiveStreamers";
+import StreamerName from "./StreamerName";
+import StreamerTitle from "./StreamerTitle";
 const MostStreamers = () => {
   return (
-    <div className="flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16">
+    <div
+      className="flex items-center justify-center flex-col bg-[#1f2122] rounded-3xl mx-auto mt-16"
+      data-aos="fade-up"
+      data-aos-delay="500"
+      data-aos-offset="150"
+    >
       <div className="p-8 w-full flex flex-col items-center justify-center">
         <Title Text1="Most Popular" Text2="Live Stream" />
         <div className="w-full gap-5 pb-6 grid sm:grid-cols-1 sl:grid-cols-2 md:grid-cols-4 mt-6">
           {Streamers.map((card) => (
             <div className="w-full h-full" key={card.id}>
               {/* Top Div */}
-              <div className="relative group">
+              <div
+                className="relative group"
+                data-aos="fade-up"
+                data-aos-delay="600"
+                data-aos-offset="200"
+              >
                 <img src={card.image} alt="" className="rounded-3xl w-full" />
                 <div className="opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-500">
-                  <div className="absolute top-4 right-5">
-                    <Link to={"/streams"} className="btn-s text-">
-                      Live
-                    </Link>
-                  </div>
-                  <div className="absolute bottom-4 w-full flex items-center justify-center gap-x-3">
-                    <Link
-                      to={"/streams"}
-                      className="btn-s flex items-center justify-center gap-x-1"
-                    >
-                      <span className="text-xl">
-                        <FaRegEye />
-                      </span>
-                      1.2K
-                    </Link>
-                    <Link
-                      to={"/streams"}
-                      className="btn-s flex items-center justify-center gap-x-1"
-                    >
-                      <span className="text-2xl">
-                        <IoLogoGameControllerA />
-                      </span>
-                      CS-GO
-                    </Link>
-                  </div>
+                  <Live link="/streams" name="Live" />
+                  <ActiveStreamers link="/streams" view="1.2K" name="CS-GO" />
                 </div>
               </div>
               {/* Bottom Div */}
-              <div className="pt-6 flex w-full justify-start items-start">
+              <div
+                className="pt-6 flex w-full justify-start items-start"
+                data-aos="fade-up"
+                data-aos-delay="600"
+                data-aos-offset="280"
+              >
                 {/* Left Div */}
-                <div className="">
+                <div>
                   <img
                     src={card.profileImg}
                     alt=""
@@ -58,29 +49,22 @@ const MostStreamers = () => {
                 </div>
                 {/* Right Div */}
                 <div className="flex flex-col ml-3 items-start justify-start">
-                  <div className="flex items-center justify-start">
-                    <span className="bg-[#ec6090] rounded-full items-center mr-2 text-xl font-thin">
-                      <BiCheck />
-                    </span>
-                    <h3 className="text-[#ec6090] text-sm">{card.name}</h3>
-                  </div>
-                  <div className="py-2 mr-2">
-                    <h3 className="text-xl font-bold flex-wrap ">
-                      {card.title}
-                    </h3>
-                  </div>
+                  <StreamerName name={card.name} />
+                  <StreamerTitle title={card.title} />
                 </div>
               </div>
             </div>
           ))}
         </div>
-
-        <div className="-mb-14">
+        <div
+          className="-mb-14"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           <Button link="/streams" name="Load More Streams" />
         </div>
       </div>
     </div>
   );
 };
-
 export default MostStreamers;
