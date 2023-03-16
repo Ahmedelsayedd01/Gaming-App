36cf214e8d6920d4d272a3a5f5f7b51880eacd5a
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-16 23:46:10 +0200

    New Update

---
+++ src/components/Rate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StarIcon from "./StarIcon";
+
+const Rate = (props) => {
+  return (
+    <ul className="flex items-center  text-white text-base font-light">
+      <StarIcon />
+      <li>{props.rate}</li>
+    </ul>
+  );
+};
+
+export default Rate;
