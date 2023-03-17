015ce2c3bb34cbc2e01c1fd722f1368d1eeb97db
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-17 23:51:50 +0200

    New Update

---
+++ src/components/navbar/NavDesk.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavDesk = (props) => {
+  return (
+    <NavLink
+      to={props.link}
+      className="text-[#666] font-medium transition-all ease-in-out duration-300 hover:text-[#e75e8d] hover: focus:text-[#e75e8d] py-2 "
+    >
+      {props.name}
+    </NavLink>
+  );
+};
+
+export default NavDesk;
