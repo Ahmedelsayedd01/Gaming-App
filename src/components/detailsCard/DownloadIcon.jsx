94747f14c811084a530648b34fd554d88e83ec00
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-17 23:50:58 +0200

    New Update

---
+++ src/components/detailsCard/DownloadIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaDownload } from "react-icons/fa";
+
+const DownloadIcon = () => {
+  return (
+      <FaDownload  className="text-[#ec6090] mr-1"/>
+  );
+};
+
+export default DownloadIcon;
