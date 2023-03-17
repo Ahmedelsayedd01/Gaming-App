52e6117d5c07324a0eaf60c87343520522b24d49
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-17 23:52:02 +0200

    New Update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="w-full sm:my-11 sm:px-4 flex items-center justify-center"
+      data-aos="zoom-in-right"
+      data-aos-delay="500"
+      data-aos-offset="50"
+    >
+      <ul className="sm:flex sl:my-6 flex-col items-center">
+        <li className="w-full sm:leading-6 sm:text-sm sl:text-lg leading-10 font-light sl:flex">
+          Copyright<span className="text-[#ec6090] mx-1">&copy;</span>2023
+          Cyborg Gaming Company. All rights reserved.
+        </li>
+        <li className="text-lg font-light">
+          Design:
+          <span className="font-light text-[#ec6090]"> Ahmed Elsayed</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
