666467807c29e7103fffe266c7694ea3fc137458
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-19 17:43:09 +0200

    New Update

---
+++ src/components/piecesOfComponent/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <Link to={"/"} className=" md:border-r md:border-[#27292a] pr-2 md:pr-14">
+      <img src={logo} alt="" className="" />
+    </Link>
+  );
+};
+
+export default Logo;
