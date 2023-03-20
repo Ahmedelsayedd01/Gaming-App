e2902ddf0eec4a09a58aeebfdac46be9d0b74268
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-20 22:27:27 +0200

    New Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Header from "../components/Header";
-import SectionLibrary from "../components/SectionLibrary";
-import SectionRight from "../components/SectionRight";
+/* Import Components */
+import Header from "../components/pagesHeader/HomeHeader";
+import Library from "../components/sections/Library";
+import Right from "../components/sections/Right";
 
 const Home = () => {
   return (
     <>
       <div className="w-10/12 mx-auto mt-4">
         <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-14">
-          {/* casteism Section */}
           <Header />
-          <SectionRight />
-          <SectionLibrary />
+          <Right />
+          <Library />
         </div>
       </div>
     </>
