459304b0d871ca24b4e8771905c3ea4fb3a4d038
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-20 22:27:09 +0200

    New Update

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import DetailsGames from "../components/DetailsGames";
-import HeaderDetails from "../components/HeaderDetails";
-import SelectionDetails from "../components/SelectionDetails";
+/* Import Components */
+import DetailsGames from "../components/sections/DetailsGames";
+import HeaderDetails from "../components/pagesHeader/DetailsHeader";
+import Fortnite from "../components/sections/Fortnite";
 
 const Details = () => {
   return (
     <>
       <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
-        <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
+        <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12 overflow-hidden">
+          {/* Sections */}
           <HeaderDetails />
-          <SelectionDetails />
+          <Fortnite />
           <DetailsGames />
         </div>
       </div>
