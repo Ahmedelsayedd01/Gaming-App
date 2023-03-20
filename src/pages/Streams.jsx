2537f7764579131f2ba2acfc982730b795f0a6b4
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-20 22:28:07 +0200

    New Update

--- src/pages/Streams.jsx
+++ src/pages/Streams.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import LiveStreams from "../components/LiveStreams";
-import MostStreamers from "../components/MostStreamers";
-import TopStreams from "../components/TopStreams";
+import LiveStreams from "../components/sections/LiveStreams";
+import MostLiveStream from "../components/sections/MostLiveStream";
+import TopStreams from "../components/sections/TopStreams";
 
 const Streams = () => {
   return (
@@ -9,14 +9,14 @@ const Streams = () => {
       <div className="sm:w-11/12 md:w-10/12 mx-auto mt-4">
         <div className="bg-[#27292a] rounded-3xl mx-auto sm:px-6 sl:p-12">
           <div className="flex sm:flex-col md:flex-row md:gap-x-5 w-full items-center md:justify-between ">
-            <div className="sm:w-full md:w-4/6">
+            <div className="sm:w-full md:w-4/6 overflow-hidden">
               <LiveStreams />
             </div>
-            <div className="sm:w-full md:w-2/6">
+            <div className="sm:w-full md:w-2/6 overflow-hidden">
               <TopStreams />
             </div>
           </div>
-            <MostStreamers />
+          <MostLiveStream />
         </div>
       </div>
     </>
