54018b499fdccd2cd129411656a23b640459c5d4
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-23 23:06:43 +0200

    Last Update

--- src/components/detailsCard/StarIcon.jsx
+++ src/components/detailsCard/StarIcon.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
 const StarIcon = () => {
-  return (
-      <AiFillStar className="text-yellow-300 mr-1"/>
-  );
+  return <AiFillStar className="text-yellow-300 mr-1" />;
 };
 
 export default StarIcon;
