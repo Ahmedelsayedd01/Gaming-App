0a748f3c5dd1ff2b1c99be03d3c4b3f651628d11
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-24 18:56:25 +0200

    New Update

--- src/data.js
+++ src/data.js
@@ -19,12 +19,12 @@ import Featured3 from "./assets/images/featured-03.jpg";
 import LiveStream1 from "./assets/images/service-01.jpg";
 import LiveStream2 from "./assets/images/service-02.jpg";
 import LiveStream3 from "./assets/images/service-03.jpg";
-/* Import Photo LiveStreamGames From Images  */
+/* Import Photo stream From Images  */
 import LiveStreamGames1 from "./assets/images/stream-01.jpg";
 import LiveStreamGames2 from "./assets/images/stream-02.jpg";
 import LiveStreamGames3 from "./assets/images/stream-03.jpg";
 import LiveStreamGames4 from "./assets/images/stream-04.jpg";
-
+/* Import Photo avatar From Images  */
 import profile1 from "./assets/images/avatar-01.jpg";
 import profile2 from "./assets/images/avatar-02.jpg";
 import profile3 from "./assets/images/avatar-03.jpg";
